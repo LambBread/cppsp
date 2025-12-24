69f51cde7010a3305deeee223e61a12fcd8f81d9
LambBread <judahjohnson4702@gmail.com>
2025-12-24 10:57:15 -0800

    Update CMakeLists.txt

--- src/main.cpp
+++ src/main.cpp
@@ -423,8 +423,7 @@ int main(int argc, char* argv[])
     ////////
     if (argc < 2)
     {
-        std::cerr
-                << "Usage: cppsp_compiler <file>\n";
+        std::cerr << "Usage: cppsp_compiler <file>\n";
         return 1;
     }
 
